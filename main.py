13ad9d32e047b4ae5a90f3e063c8a799da2ff969
reflect9 <reflect9@gmail.com>
2018-05-31 01:06:47 -0700

    haha

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
+import os
+import urllib
+
+from google.appengine.api import users
+from google.appengine.ext import ndb
+
+import jinja2
 import webapp2
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello, World!')
 
+class EssayPage(webapp2.RequestHandler):
+    """ """ 
+    def get(self):
+        """ """
+        essay_id = self.request.get("id")
+        template = JINJA_ENVIRONMENT.get_template('essay/'+str(essay_id)+'.html')
+        self.response.write(template.render({   }))
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/essay', EssayPage),
 ], debug=True)
